24a6c280ee361d12fd176bacb20ab538f0556b37
Lucas Bueno <lucas-jbb@hotmail.com>
2022-04-11 16:55:19 -0300

    feat: change js to ts files

---
+++ src/components/FoodCard/index.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import { FiEdit3, FiTrash } from 'react-icons/fi';
+
+import { Container } from './styles';
+import api from '../../services/api';
+import { Food } from 'models/Food';
+
+interface FoodCardProps {
+  food: Food
+  handleDeleteFood: (id: number) => Promise<void>
+  handleEditFood: (food: Food) => void
+}
+
+function FoodCard({
+  food,
+  handleDeleteFood,
+  handleEditFood
+}: FoodCardProps) {
+  const [available, setAvailable] = useState(false)
+
+  const toggleAvailable = async () => {
+    await api.put(`/foods/${food.id}`, {
+      ...food,
+      available: !available,
+    });
+    setAvailable(!available)
+  }
+
+  const setEditingFood = () => {
+    handleEditFood(food);
+  }
+
+  return (
+    <Container available={available}>
+      <header>
+        <img src={food.image} alt={food.name} />
+      </header>
+      <section className="body">
+        <h2>{food.name}</h2>
+        <p>{food.name}</p>
+        <p className="price">
+          R$ <b>{food.price}</b>
+        </p>
+      </section>
+      <section className="footer">
+        <div className="icon-container">
+          <button
+            type="button"
+            className="icon"
+            onClick={setEditingFood}
+            data-testid={`edit-food-${food.id}`}
+          >
+            <FiEdit3 size={20} />
+          </button>
+
+          <button
+            type="button"
+            className="icon"
+            onClick={() => handleDeleteFood(food.id)}
+            data-testid={`remove-food-${food.id}`}
+          >
+            <FiTrash size={20} />
+          </button>
+        </div>
+
+        <div className="availability-container">
+          <p>{available ? 'Disponível' : 'Indisponível'}</p>
+
+          <label htmlFor={`available-switch-${food.id}`} className="switch">
+            <input
+              id={`available-switch-${food.id}`}
+              type="checkbox"
+              checked={available}
+              onChange={toggleAvailable}
+              data-testid={`change-status-food-${food.id}`}
+            />
+            <span className="slider" />
+          </label>
+        </div>
+      </section>
+    </Container>
+  );
+};
+
+export default FoodCard;
